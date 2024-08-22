f6f7077c1c83b367ec0bf916af1d8d4afdc17434
Valentine <vals91@outlook.com>
2024-08-22 22:11:45 +0300

    second commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 2600;
+
+        int bonus = ticketPrice / 20;
+        System.out.println("Бонус: " + bonus);
+    }
+}
